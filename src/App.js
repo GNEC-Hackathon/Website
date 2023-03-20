c4cae13d326c18e34295f0657de13c2837ca83a0
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-03-20 03:18:49 -0400

    try react three js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import { MDBBtn, MDBContainer } from 'mdb-react-ui-kit';
+import { Canvas } from '@react-three/fiber';
+import { Box, OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
-    <MDBContainer fluid>
-      <div
-        className='d-flex justify-content-center align-items-center'
-        style={{ height: '100vh' }}
-      >
-        <div className='text-center'>
-          <img
-            className='mb-4'
-            src='https://mdbootstrap.com/img/logo/mdb-transparent-250px.png'
-            style={{ width: 250, height: 90 }}
-          />
-          <h5 className='mb-3'>
-            Thank you for using our product. We're glad you're with us.
-          </h5>
-          <p className='mb-3'>MDB Team</p>
-          <MDBBtn
-            tag='a'
-            href='https://mdbootstrap.com/docs/standard/getting-started/'
-            target='_blank'
-            role='button'
-          >
-            Start MDB tutorial
-          </MDBBtn>
-        </div>
-      </div>
-    </MDBContainer>
+    <Canvas>
+      <ambientLight />
+      <Box position={[0, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
   );
 }
 
