fc0ecf10079287c9c2d3de3689d70cafdb4bed80
Mootbing <50122069+Mootbing@users.noreply.github.com>
2023-03-31 10:43:09 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { MDBAnimatedNavbar, MDBBtn, MDBContainer, MDBNavbarLink } from 'mdb-react-ui-kit';
+import { MDBAnimatedNavbar, MDBBtn, MDBContainer, MDBNavbarLink, MDBCol, MDBRow} from 'mdb-react-ui-kit';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { Box, OrbitControls } from '@react-three/drei';
 
@@ -9,67 +9,80 @@ import GlobeModel from './components/GlobeModel';
 function App() {
   return (
     <div>
-      <NavBar />
-      <Canvas style={{height: "50vh", width: "50vw", backgroundColor: "transparent", overflow: "show"}}>
+      <Canvas className="home" style={{width: "100%", backgroundColor: "transparent", overflow: "show", height: "100vh"}}>
         <ambientLight />
-        <GlobeModel position={[0, -2, 0]} scale={[0.04, 0.04, 0.04]}/>
+        <GlobeModel position={[0, -2.1, 0]} scale={[0.04, 0.04, 0.04]}/>
         <OrbitControls enableZoom={false} enablePan={false}/>
       </Canvas>
-      <p style={{position: "absolute", left: "50vw", top: "23vh", fontFamily: "Montserrat", fontSize: 40}}>Connecting Through Doing</p>
-      <img src={"./assets/images/TopTransition.png"} style={{width: "100%", marginTop: -250}}/>
-      <div style={{width: "100%", backgroundColor:  "#171717", padding: 50, marginTop: -250}}>
-        <div style={{flexDirection: "row",  display: "flex"}}>
-          <div style={{width: "50%", padding: 50}}>
+      <div style={{height: "50vh"}}/>
+      <div style={{position: "absolute", top: "40vh", left: 50, maxWidth: 300}}>
+        <p style={{fontFamily: "Montserrat", fontSize: "2rem", color: "white", fontWeight: "bold", color: "#fff",
+        textShadow: "0px 0px 10px rgba(0, 0, 0, 1)"}}>
+          {"< Connecting Through Doing />"}
+        </p>
+      </div>
+      <img src={"./assets/images/TopTransition.png"} style={{width: "100%", marginTop: -600}}/>
+      <div className='about' style={{width: "100%", backgroundColor:  "#171717", padding: 50, marginTop: -350}}>
+        <MDBRow style={{
+            width: "100%",
+            justifyContent: "center",
+          }}>
+          <MDBCol size='12' sm='5'>
             <h1 style={{fontFamily: "Montserrat", fontSize: 40, color: "white", fontWeight: "bold"}}>ABOUT</h1>
             <p style={{fontFamily: "Montserrat", color: "#A4E9FF", fontWeight: "bold"}}>GNEC Hackathon</p>
             <p style={{fontFamily: "Montserrat", color: "#D5D5D5"}}>This hackathon is hosted by GNEC (Global NGO Executive Community), which promotes connections between non-profit and non-governmental organizations and help them find sponsors. Through our hackathon, we hope to award those who create innovative solutions while simultaneously look for talents to join our team.</p>
-          </div>
-          <div style={{width: "50%", padding: 50}}>
+          </MDBCol>
+          <MDBCol size='12' sm='5'>
             <img src={"./assets/images/Main.png"} style={{width: "100%"}}/>
-          </div>
-        </div>
-        <center>
-          <h1 style={{fontFamily: "Montserrat", fontSize: 40, color: "white", fontWeight: "bold", margin: 100}}>RULES</h1>
-          <div style={{flexDirection: "row",  display: "flex"}}>
-          <div style={{width: "30%", paddingLeft: 50, paddingRight: 25}}>
-            <h1 style={{fontFamily: "Montserrat", marginBottom: 30, fontSize: 25, color: "#A4E9FF", fontWeight: "bold", textAlign: "left"}}>Submission Guidelines</h1>
-            <ol style={{color: "#D5D5D5", fontSize: 14, fontFamily: "Montserrat"}}>
-              <li style={{marginBottom: 15}}>Each team must submit a 5 minute or less Youtube video.</li>
-              <li style={{marginBottom: 15}}>Code should be submitted to Github and remain public.</li>
-              <li style={{marginBottom: 15}}>Cross submission are NOT allowed.</li>
-              <li>Resubmission and prior work are NOT allowed.</li>
-            </ol>
-          </div>
-          <div style={{width: "30%", paddingLeft: 25, paddingRight: 25}}>
-            <h1 style={{fontFamily: "Montserrat", marginBottom: 30, fontSize: 25, color: "#A4E9FF", fontWeight: "bold", textAlign: "left"}}>Topic & Theme</h1>
-            <ol style={{color: "#D5D5D5", fontSize: 14, fontFamily: "Montserrat"}}>
-              <li style={{marginBottom: 15}}>The general goal of this hackathon is to promote connections among non-profit and non-governmental organizations and help them find sponsors.</li>
-              <li style={{marginBottom: 15}}>Specific theme & tracks will be announced in the opening ceremony.</li>
-            </ol>
-          </div>
-          <div style={{width: "30%", paddingRight: 50, paddingLeft: 25}}>
-            <h1 style={{fontFamily: "Montserrat", marginBottom: 30, fontSize: 25, color: "#A4E9FF", fontWeight: "bold", textAlign: "left"}}>Team Info</h1>
-            <ol style={{color: "#D5D5D5", fontSize: 14, fontFamily: "Montserrat"}}>
-              <li style={{marginBottom: 15}}>Team members should be no more than five people.</li>
-              <li style={{marginBottom: 15}}>College students from all over the world are welcome to attend.</li>
-              <li>You may group up by yourself, or attend our group formation event to find your teammates.</li>
-            </ol>
-          </div>
-        </div>
+          </MDBCol>
+        </MDBRow>
+        <center className='rules'>
+          <h1 style={{fontFamily: "Montserrat", fontSize: 40, color: "white", fontWeight: "bold", marginTop: 100, marginBottom: 25}}>RULES</h1>
+          <MDBRow style={{
+            width: "100%",
+            justifyContent: "center",
+          }}>
+            <MDBCol size='6' sm='3'>
+              <h1 style={{fontFamily: "Montserrat", marginBottom: 30, fontSize: 25, color: "#A4E9FF", fontWeight: "bold", textAlign: "left"}}>Submission Guidelines</h1>
+              <ol style={{color: "#D5D5D5", fontSize: 14, fontFamily: "Montserrat"}}>
+                <li style={{marginBottom: 15}}>Each team must submit a 5 minute or less Youtube video.</li>
+                <li style={{marginBottom: 15}}>Code should be submitted to Github and remain public.</li>
+                <li style={{marginBottom: 15}}>Cross submission are NOT allowed.</li>
+                <li>Resubmission and prior work are NOT allowed.</li>
+              </ol>
+            </MDBCol>
+            <MDBCol size='6' sm='3'>
+              <h1 style={{fontFamily: "Montserrat", marginBottom: 30, fontSize: 25, color: "#A4E9FF", fontWeight: "bold", textAlign: "left"}}>Topic & Theme</h1>
+              <ol style={{color: "#D5D5D5", fontSize: 14, fontFamily: "Montserrat"}}>
+                <li style={{marginBottom: 15}}>The general goal of this hackathon is to promote connections among non-profit and non-governmental organizations and help them find sponsors.</li>
+                <li style={{marginBottom: 15}}>Specific theme & tracks will be announced in the opening ceremony.</li>
+              </ol>
+            </MDBCol>
+            <MDBCol size='6' sm='3'>
+              <h1 style={{fontFamily: "Montserrat", marginBottom: 30, fontSize: 25, color: "#A4E9FF", fontWeight: "bold", textAlign: "left"}}>Team Info</h1>
+              <ol style={{color: "#D5D5D5", fontSize: 14, fontFamily: "Montserrat"}}>
+                <li style={{marginBottom: 15}}>Team members should be no more than five people.</li>
+                <li style={{marginBottom: 15}}>College students from all over the world are welcome to attend.</li>
+                <li>You may group up by yourself, or attend our group formation event to find your teammates.</li>
+              </ol>
+            </MDBCol>
+        </MDBRow>
         </center>
-        <img src={"./assets/images/Slate.png"} style={{width: "100%", maxWidth: 1000, marginLeft: -50}}/>
+        <img className='prizes' src={"./assets/images/Slate.png"} style={{width: "100%", maxWidth: 1000, marginLeft: -50}}/>
       </div>
-      <div style={{width: "100%", backgroundColor: "#171717"}}>
+      <center className='roadmap' style={{width: "100%", backgroundColor: "#171717"}}>
+        <h1 style={{fontFamily: "Montserrat", fontSize: 40, color: "white", fontWeight: "bold", marginBottom: 25}}>ROADMAP</h1>
         <img src={"./assets/images/Roadmap.png"} style={{width: "100%", maxWidth: 1000}}/>
-      </div>
+      </center>
       <center style={{backgroundColor: "#fff", width: "100%", padding: 100}}>
       <p style={{fontFamily: "Montserrat", color: "#A4E9FF", fontWeight: "bold"}}>Thank you to our</p>
-        <h1 style={{fontFamily: "Montserrat", fontSize: 40, color: "#171717", fontWeight: "bold", marginBottom: 50}}>SPONSORS</h1>
+        <h1 className='sponsors' style={{fontFamily: "Montserrat", fontSize: 40, color: "#171717", fontWeight: "bold", marginBottom: 50}}>SPONSORS</h1>
         <img src={"./assets/images/Sponsors.png"} style={{width: "100%"}}/>
         <p style={{fontFamily: "Montserrat", color: "#A4E9FF", fontWeight: "bold", marginTop: 150}}>Thank you to our</p>
-        <h1 style={{fontFamily: "Montserrat", fontSize: 40, color: "#171717", fontWeight: "bold", marginBottom: 50}}>Partners</h1>
+        <h1 className='partners' style={{fontFamily: "Montserrat", fontSize: 40, color: "#171717", fontWeight: "bold", marginBottom: 50}}>Partners</h1>
         <img src={"./assets/images/Partners.png"} style={{width: "100%"}}/>
       </center>
+      <NavBar />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -20,8 +20,16 @@ import {
 export default function App() {
   const [showBasic, setShowBasic] = useState(false);
 
+  const [windowScrollY, setWindowScrollY] = useState(0);
+
+  window.addEventListener("scroll", () => {
+    setWindowScrollY(window.scrollY);
+  });
+
   return (
-    <MDBAnimatedNavbar style={{boxShadow: "none"}} expand='lg' light bgColor='light'>
+    <MDBAnimatedNavbar style={{zIndex: 1e101, position: "fixed", top: 0, width: "100%", boxShadow: windowScrollY == 0 ? "none" : 
+    "0px 0px 10px 0px rgba(0, 0, 0, 0.2)"
+    , backgroundColor: "#fff"}} light expand='lg'>
       <MDBContainer fluid>
         <MDBNavbarBrand href='#'>
             <img src='./assets/images/gnec.png' alt='logo' width={150}/>
@@ -31,30 +39,43 @@ export default function App() {
           aria-controls='navbarSupportedContent'
           aria-expanded='false'
           aria-label='Toggle navigation'
+          style={{width: 50, height: 50, borderRadius: 50, borderWidth: 1, border: "1px solid #000", justifyContent: "center", alignItems: "center", zIndex: 1e10}}
           onClick={() => setShowBasic(!showBasic)}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon='bars' style={{color: "#121212"}} fas />
         </MDBNavbarToggler>
 
         <MDBCollapse navbar center show={showBasic}>
-          <MDBNavbarNav style={{justifyContent: "center"}} className='mb-2 mb-lg-0'>
+          <MDBNavbarNav style={{justifyContent: "center", zIndex: 1e100}} className='mb-2 mb-lg-0'>
             <MDBNavbarItem>
-              <MDBNavbarLink active href='#' className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}}>
+              <MDBNavbarLink active className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}} onClick={() => {
+                const y = document.getElementsByClassName("home")[0].offsetTop;
+                window.scrollTo({top: y, behavior: "smooth"});
+              }}>
                 Home
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-                <MDBNavbarLink active href='#' className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}}>
+                <MDBNavbarLink active className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}} onClick={() => {
+                  const y = document.getElementsByClassName("about")[0].offsetTop;
+                  window.scrollTo({top: y, behavior: "smooth"});
+                }}>
                     About
                 </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-                <MDBNavbarLink active href='#' className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}}>
+              <MDBNavbarLink active className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}} onClick={() => {
+                const y = document.getElementsByClassName("prizes")[0].offsetTop;
+                window.scrollTo({top: y, behavior: "smooth"});
+              }}>
                     Prizes
                 </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-                <MDBNavbarLink active href='#' className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}}>
+              <MDBNavbarLink active className="me-4" style={{fontFamily: "Montserrat", fontSize: 14}} onClick={() => {
+                const y = document.getElementsByClassName("roadmap")[0].offsetTop;
+                window.scrollTo({top: y, behavior: "smooth"});
+              }}>
                     Roadmap
                 </MDBNavbarLink>
             </MDBNavbarItem>
